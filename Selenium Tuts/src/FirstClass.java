159ec94b242826d6b701b5a3b813dbf74392a85b
deepender <deepenrathore28@gmail.com>
2017-06-17 18:47:58 +0530

    SetProperty for dirver / open a web page using get()

--- Selenium Tuts/src/FirstClass.java
+++ Selenium Tuts/src/FirstClass.java
@@ -1,11 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","/home/deep/Downloads/geckodriver");
 		
-		System.out.println("Deepender");
-		System.out.println("Test");
+		WebDriver driver;
+		
+		driver = new FirefoxDriver();
+		
+		driver.get("https://www.google.com");
 
 	}
 
