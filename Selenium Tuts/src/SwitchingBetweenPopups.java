3a502f604ed34649f4393ca162b4bbef8298830a
deepender <deepenrathore28@gmail.com>
2017-06-28 23:24:29 +0530

    Switching Between Pop-up Windows

---
+++ Selenium Tuts/src/SwitchingBetweenPopups.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SwitchingBetweenPopups {
+	
+	public static void main(String args[]){
+		
+		System.setProperty("webdriver.gecko.driver", "/home/deep/Downloads/geckodriver");
+		
+		WebDriver driver;
+		
+		driver = new FirefoxDriver();
+		
+		driver.get("http://jsbin.com/usidix/1");
+		
+		//driver.findElement(By.cssSelector("input[value=\"Go!\"]")).click();
+		driver.findElement(By.tagName("input")).click();
+		
+		String alertmsg = driver.switchTo().alert().getText();
+		
+		driver.switchTo().alert().accept();
+		
+		System.out.println(alertmsg);
+		driver.quit();
+	}
+
+}
