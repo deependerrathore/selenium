215d66306aead7f9c121f1bbe826b761db1459ab
deepender <deepenrathore28@gmail.com>
2017-07-17 19:03:35 +0530

    Get number of rows and columns of Dynamic Table

--- Selenium Tuts/src/ReadDynamicTable.java
+++ Selenium Tuts/src/ReadDynamicTable.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,11 +14,17 @@ public class ReadDynamicTable {
 		
 		driver = new FirefoxDriver();
 		
-		driver.get("http://money.rediff.com/gainers/bsc/daily/groupa");
+		driver.get("http://money.rediff.com/gainers/bsc/daily/groupb");
 		
 		//List of columns
-		WebElement tab = driver.findElement(By.xpath(".//*[@id='leftcontainer']/table/thead/tr/th"));
-	
-		System.out.println(tab.getText());
+		List <WebElement> col = driver.findElements(By.xpath(".//*[@id='leftcontainer']/table/thead/tr/th"));
+		
+		System.out.println("Number of cols are :" + col.size());
+		
+		//List of rows
+		List <WebElement> row = driver.findElements(By.xpath(".//*[@id='leftcontainer']/table/tbody/tr"));
+		
+		System.out.println("Number of rows are: "+ row.size());
+		
 	}
 }
