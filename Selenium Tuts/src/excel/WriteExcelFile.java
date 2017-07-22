8c80a8313cedbadf49a6dd893ffed1f8b8b77292
deepender <deepenrathore28@gmail.com>
2017-07-22 17:15:24 +0530

    Write to excel file - XLSX is successfully written - Problem with XLS - need to try in windows

--- Selenium Tuts/src/excel/WriteExcelFile.java
+++ Selenium Tuts/src/excel/WriteExcelFile.java
@@ -2,16 +2,18 @@ package excel;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteExcelFile {
-	public void writeToExcelFile(String filePath, String fileName, String sheetName) throws IOException {
+	public void writeToExcelFile(String filePath, String fileName, String sheetName,String[] dataToWrite) throws IOException {
 		
 		File file = new File(filePath+"/"+fileName);
 		
@@ -26,24 +28,47 @@ public class WriteExcelFile {
 		} else if(excelFileExtension.equals(".xls")){
 			excelDemoFile = new HSSFWorkbook(fis);
 		}else {
-			IOException io = new IOException("Invalid file extension");
+			System.out.println("Invalid file exceltion");
+			//IOException io = new IOException("Invalid file extension");
 		}
 		
 		Sheet excelDemoSheet = excelDemoFile.getSheet(sheetName);
 		
 		int rowCount = excelDemoSheet.getLastRowNum() - excelDemoSheet.getFirstRowNum();
-		for (int i = 0; i < rowCount; i++) {
-			Row row = excelDemoSheet.getRow(i);
-			for (int j = 0; j < row.getLastCellNum(); j++) {
-				System.out.print(row.getCell(j).getStringCellValue()+"||");
-			}
-			System.out.println();
+		
+		//Get the first row from the sheet
+		Row row = excelDemoSheet.getRow(0);
+		
+		
+		//Create a new row and append it at last of sheet
+		Row newRow = excelDemoSheet.createRow(rowCount+1);
+		
+		//Create a loop over the cell of newly created Row
+		for (int i = 0; i < row.getLastCellNum(); i++) {
+			//Fill data to last row
+			Cell cell = newRow.createCell(i);
+			cell.setCellValue(dataToWrite[i]);
 		}
 		
+		//Close Input Stream
+		fis.close();
+		
+		//Create an object of FileOutputStream class to create write data in excel file
+		FileOutputStream fos = new FileOutputStream(file);
+		
+		//Write data to excel file
+		excelDemoFile.write(fos);
+		
+		//Close the output stream
+		fos.close();
 	}
 	public static void main(String[] args) throws IOException {
 		WriteExcelFile wef = new WriteExcelFile();
-		String filePath = System.getProperty("user.dir")+"/src/excel";
-		wef.writeToExcelFile(filePath, "ExcelDemoFile.xls", "Sheet1");
+		String filePath = System.getProperty("user.dir")+"/src/excel/";
+		
+		//Create an array with the data in the same order in which you expect to be filled in excel file
+
+        String[] valueToWrite = {"Rahul","Engineer"};
+		wef.writeToExcelFile(filePath, "Exceldata.xlsx", "Sheet1",valueToWrite);
 	}
 }
